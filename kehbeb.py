8b7c6662475def4d226612cdf56cd6e1f8978202
Polo <53773040+polowiper@users.noreply.github.com>
2023-08-21 12:56:57 +0200

    issou
    le bot

---
+++ kehbeb.py
@@ -0,0 +1,73 @@
+import discord
+from discord.ext import commands
+import asyncio
+
+intents = discord.Intents.default()
+intents.members = True
+intents.message_content = True
+intents.messages = True
+
+
+bote = commands.Bot(command_prefix='!', selfbot = False, intents=intents)
+
+@bote.event
+async def on_message(msg):
+    num = 0
+    decimal = False
+    decinum = 1
+    undecinum = 1
+    bruh = []
+    if '€' in msg.content:
+        for aya in msg.content:
+            if aya.isdigit():
+                if decimal == False:
+                    if aya == 0:
+                        num = float(num + str(aya))
+                        undecinum = 10
+                    else: 
+                        num = float(num) * (undecinum) 
+                        num = float(num + float(aya))
+                        undecinum = 10 
+                if decimal == True:
+                    if aya == 0:
+                        num = float(num + str(aya))
+                        decinum *= 10
+                    else:
+                        aya = float(aya) / (10 * decinum)
+                        num = float(num + float(aya))
+                        decinum *= 10
+            if "," in str(aya):
+                decimal = True
+            if aya == "€":
+                bruh.append(num)
+                decinum = 1
+                undecinum = 1
+                decimal = False
+                num = 0
+                pass
+        po = 0
+        for i in bruh:
+            bruh[po] = round((i / 7.86), 5)
+            po += 1
+        if len(bruh) <= 1:
+            if bruh[0] < 1:
+                await msg.channel.send("wallah c " + str(bruh[0]) + " kehbebs chef t trop pauvre la honte")
+            elif bruh[0] > 5 and bruh[0] < 10:
+                await msg.channel.send("wallah c " + str(bruh[0]) + " kehbebs chef tu khalass tes potes ou quoi?")
+            elif bruh[0] > 10:
+                await msg.channel.send("wallah c " + str(bruh[0]) + " kehbebs chef t Elon Musk wsh")
+            else:
+                await msg.channel.send("wallah c " + str(bruh[0]) + " kehbebs chef")    
+        if len(bruh) > 1:
+            bo = "wallah c "
+            b = 0
+            for i in bruh:
+                if b == (len(bruh) - 2):
+                    bo += str(bruh[b]) + " et "
+                else:
+                    bo += str(bruh[b]) + ", "
+                b += 1
+            bo += " kehbeb chef pfiouuuu sa fé beaucoup"
+            await msg.channel.send(bo)
+
+bote.run(BOT TOKEN)
